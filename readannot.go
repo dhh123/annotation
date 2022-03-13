b5160f164a42b7a7996f576603e38faceffffa84
段鸿辉 <duanhonghui@yinhe.ht>
2022-03-13 23:22:44 +0800

    add galaxy

--- readannot.go
+++ readannot.go
@@ -1,72 +1,78 @@
 package annotation
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net"
+	"net/http"
+	"net/url"
 	"os"
 	"strings"
-        "time"
-        "context"
-        //"github.com/containernetworking/plugins/pkg/ipam"
+	"time"
+
+	//"github.com/containernetworking/plugins/pkg/ipam"
 	"github.com/containernetworking/cni/pkg/skel"
 	"github.com/containernetworking/cni/pkg/types/current"
-         "k8s.io/apimachinery/pkg/util/wait"
 	"github.com/vishvananda/netlink"
-        "k8s.io/apimachinery/pkg/api/errors"
 	weaveapi "github.com/weaveworks/weave/api"
-        metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-        corev1 "k8s.io/api/core/v1"
-        "k8s.io/client-go/kubernetes"
-        "k8s.io/client-go/tools/clientcmd"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
+
 const (
 	K8S_POD_NAMESPACE          = "K8S_POD_NAMESPACE"
 	K8S_POD_NAME               = "K8S_POD_NAME"
 	K8S_POD_INFRA_CONTAINER_ID = "K8S_POD_INFRA_CONTAINER_ID"
-        ExtendedCNIArgsAnnotation = "k8s.v1.cni.galaxy.io/args"
+	ExtendedCNIArgsAnnotation  = "k8s.v1.cni.galaxy.io/args"
 
 	stateDir                   = "/var/lib/cni/galaxy/port"
 	PortMappingPortsAnnotation = "tkestack.io/portmapping"
+	GalaxyUrl                  = "http://galaxy-ipam.kube-system:9041"
 )
+
 type NetworkInfo struct {
-        NetworkType string
-        Args        map[string]string
-        Conf        map[string]interface{}
-        IfName      string
+	NetworkType string
+	Args        map[string]string
+	Conf        map[string]interface{}
+	IfName      string
 }
 
 var (
 	zeroNetwork = net.IPNet{IP: net.IPv4zero, Mask: net.IPv4Mask(0, 0, 0, 0)}
 	mask32      = net.IPv4Mask(0xff, 0xff, 0xff, 0xff)
 )
+
 type IPInfo struct {
 	IP      string `json:"ip"`
-	Vlan    uint16      `json:"vlan"`
-	Gateway net.IP      `json:"gateway"`
+	Vlan    uint16 `json:"vlan"`
+	Gateway net.IP `json:"gateway"`
 }
 
 type IPLIST struct {
-    Ipinfos  []*IPInfo
+	Ipinfos []*IPInfo
 }
 
 type CNIPlugin struct {
 	weave *weaveapi.Client
 }
 
-
 func NewCNIPlugin(weave *weaveapi.Client) *CNIPlugin {
 	return &CNIPlugin{weave: weave}
 }
 
 func IPInfoToResult(iplist []IPInfo) *current.Result {
-        newResult := &current.Result{}
-        for _,v := range(iplist) { 
-            ipnet,_ := netlink.ParseIPNet(v.IP)
-            newResult.IPs = append(newResult.IPs, &current.IPConfig{
-	    	Version: "4",
-                Address: *ipnet,
-             })
+	newResult := &current.Result{}
+	for _, v := range iplist {
+		ipnet, _ := netlink.ParseIPNet(v.IP)
+		newResult.IPs = append(newResult.IPs, &current.IPConfig{
+			Version: "4",
+			Address: *ipnet,
+		})
 	}
 	return newResult
 }
@@ -85,52 +91,109 @@ func parseExtendedCNIArgs(pod *corev1.Pod) (map[string]json.RawMessage, error) {
 		Common map[string]json.RawMessage `json:"common"`
 	}
 	if err := json.Unmarshal([]byte(args), &cniArgs); err != nil {
-        	logOnStderr(fmt.Errorf("getpodxxxxxxxxxxxxxxxxxxxxxx", err))
+		logOnStderr(fmt.Errorf("getpodxxxxxxxxxxxxxxxxxxxxxx", err))
 		return nil, fmt.Errorf("failed to unmarshal cni args %s: %v", args, err)
 	}
-        logOnStderr(fmt.Errorf("getpodxxxxxxxxxxxxxxxxxxxxxx", cniArgs))
+	logOnStderr(fmt.Errorf("getpodxxxxxxxxxxxxxxxxxxxxxx", cniArgs))
 	return cniArgs.Common, nil
 }
 
 func ParseCNIArgs(args string) (map[string]string, error) {
-        kvMap := make(map[string]string)
-        kvs := strings.Split(args, ";")
-        if len(kvs) == 0 {
-                return kvMap, fmt.Errorf("invalid args %s", args)
-        }
-        for _, kv := range kvs {
-                part := strings.SplitN(kv, "=", 2)
-                if len(part) != 2 {
-                        continue
-                }
-                kvMap[strings.TrimSpace(part[0])] = strings.TrimSpace(part[1])
-        }
-        return kvMap, nil
+	kvMap := make(map[string]string)
+	kvs := strings.Split(args, ";")
+	if len(kvs) == 0 {
+		return kvMap, fmt.Errorf("invalid args %s", args)
+	}
+	for _, kv := range kvs {
+		part := strings.SplitN(kv, "=", 2)
+		if len(part) != 2 {
+			continue
+		}
+		kvMap[strings.TrimSpace(part[0])] = strings.TrimSpace(part[1])
+	}
+	return kvMap, nil
+}
+func (client *weaveapi.Client) GetIpFromGalaxy(args *skel.CmdArgs) (*current.Result, error) {
+	// := url.values{}
+	//v.set("huifu", "hello world")
+	//body := ioutil.nopcloser(strings.newreader(v.encode())) //把form数据编下码
+	//clienvt := &http.client{}
+	//req, _ := http.newrequest("post", galaxyurl, body)
+
+	//req.header.set("content-type", "application/x-www-form-urlencoded; param=value") //这个一定要加，不加form的值post不过去，被坑了两小时
+	//fmt.printf("%+v\n", req)                                                         //看下发送的结构
+
+	//resp, err := client.do(req) //发送
+	//defer resp.body.close()     //一定要关闭resp.body
+	//data, _ := ioutil.readall(resp.body)
+	//fmt.println(string(data), err)
+	params := url.Values{}
+	params.Add("namespace", "default")
+	params.Add("netType", "overlay")
+	params.Add("nodeip", string(GetOutboundIP())+"/32")
+	params.Add("page", 1)
+	params.Add("size", 1)
+	requestUrlR := fmt.Sprintf(client.baseURL + "/v1/available/ip?" + params.Encode())
+	//curl 'http://172.16.8.195:9041/v1/allocation/ip?namespace=default&netType=overlay&ip=10.100.0.102&cid=weave-expose
+	respR, err := http.Get(requestUrlR)
+	if err != nil {
+		logOnStderr(fmt.Errorf("get ip", err))
+	}
+	params := url.Values{}
+	params.Add("namespace", "default")
+	params.Add("netType", "overlay")
+	params.Add("ip", floatResp.Content.IP)
+	params.Add("cid", args.ContainerID)
+	requestUrlS := fmt.Sprintf(client.baseURL + "/v1/allocation/ip?" + params.Encode())
+	respS, err := http.Get(requestUrlS)
+	if err != nil {
+		logOnStderr(fmt.Errorf("get ip", err))
+	}
+	var floatResp = api.ListIPResp
+	err = json.NewDecoder(respR.Body).Decode(&ListIPResp)
+	if err != nil {
+		logOnStderr(fmt.Errorf("get ip", err))
+	}
+	Result := &current.Result{}
+	Result.IPs = []*current.IPConfig{Version: 4, Address: &net.IPNet{IP: floatResp.Content.IP, Mask: 32}}
+	return Result, err
+
+}
+func GetOutboundIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		logOnStderr(fmt.Errorf("get local ip", err))
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP
 }
 
-func  GetIPFromAnnotation(kubeconfig string, args *skel.CmdArgs) (*current.Result, error) {
+func GetIPFromAnnotation(kubeconfig string, args *skel.CmdArgs) (*current.Result, error) {
 	var pod *corev1.Pod
-        cniArgs, err := ParseCNIArgs(args.Args)
-        if err != nil {
-             return nil,err
-        }
-        PodNamespace, _ := cniArgs[K8S_POD_NAMESPACE]
-        PodName, _ := cniArgs[K8S_POD_NAME]
+	cniArgs, err := ParseCNIArgs(args.Args)
+	if err != nil {
+		return nil, err
+	}
+	PodNamespace, _ := cniArgs[K8S_POD_NAMESPACE]
+	PodName, _ := cniArgs[K8S_POD_NAME]
 	printOnce := false
-        //config, err := rest.InClusterConfig()
-        config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-        if err != nil {
-	     return nil,err
-        }
-        client, err := kubernetes.NewForConfig(config)
-        if err != nil {
-	     return nil,err
-        }
-        logOnStderr(fmt.Errorf("get pods init configxxxxxxxxxxxxxxxxxxxxxx",err))
-        logOnStderr(fmt.Errorf("get pods init configxxxxxxxxxxxxxxxxxxxxxx",err))
-        if err != nil {
-	     return nil,err
-        }
+	//config, err := rest.InClusterConfig()
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		return nil, err
+	}
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+	logOnStderr(fmt.Errorf("get pods init configxxxxxxxxxxxxxxxxxxxxxx", err))
+	logOnStderr(fmt.Errorf("get pods init configxxxxxxxxxxxxxxxxxxxxxx", err))
+	if err != nil {
+		return nil, err
+	}
 	if err := wait.PollImmediate(time.Millisecond*500, 5*time.Second, func() (done bool, err error) {
 		pod, err = client.CoreV1().Pods(PodNamespace).Get(context.TODO(), PodName, metav1.GetOptions{})
 		if err != nil {
@@ -147,26 +210,26 @@ func  GetIPFromAnnotation(kubeconfig string, args *skel.CmdArgs) (*current.Resul
 	}); err != nil {
 		return nil, fmt.Errorf("failed to get pod %s_%s: %v", PodName, PodNamespace, err)
 	}
-        if err != nil {
-                return nil,err
-        }
-        extendedCNIArgs, err := parseExtendedCNIArgs(pod)
-        var infos []IPInfo
-        for k, v := range extendedCNIArgs {
-                if k == "ipinfos" {
-                        if err:=json.Unmarshal(v, &infos);err!=nil {
-                                logOnStderr(fmt.Errorf("getpod2xxxxxxxxxxxxxxxxxxxxxxargs",err))
-                        }
-                }
-                logOnStderr(fmt.Errorf("getpod2xxxxxxxxxxxxxxxxxxxxxxargs",k, v))
-        }
-		if len(infos) == 0 {
-        	return nil, nil
-		} else {
-	        newResult := IPInfoToResult(infos)
-            return newResult, nil
-
+	if err != nil {
+		return nil, err
+	}
+	extendedCNIArgs, err := parseExtendedCNIArgs(pod)
+	var infos []IPInfo
+	for k, v := range extendedCNIArgs {
+		if k == "ipinfos" {
+			if err := json.Unmarshal(v, &infos); err != nil {
+				logOnStderr(fmt.Errorf("getpod2xxxxxxxxxxxxxxxxxxxxxxargs", err))
+			}
 		}
+		logOnStderr(fmt.Errorf("getpod2xxxxxxxxxxxxxxxxxxxxxxargs", k, v))
+	}
+	if len(infos) == 0 {
+		return nil, nil
+	} else {
+		newResult := IPInfoToResult(infos)
+		return newResult, nil
+
+	}
 }
 
 func logOnStderr(err error) {
