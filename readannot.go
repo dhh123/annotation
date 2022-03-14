455bb59c25c1097deb4822d7c307218dd66cc26f
段鸿辉 <duanhonghui@yinhe.ht>
2022-03-15 00:08:17 +0800

    debug

--- readannot.go
+++ readannot.go
@@ -189,8 +189,11 @@ func GetIpFromGalaxy(args *skel.CmdArgs) (*current.Result, error) {
 	}
 	Result := &current.Result{}
 	Result.IPs = []*current.IPConfig{
+    {
 		Version: "4",
-		Address: &net.IPNet{IP: floatResp.Content[0].IP, Mask: net.IPv4Mask(255,255,255,255)}}
+		Address: &net.IPNet{IP: floatResp.Content[0].IP, Mask: net.IPv4Mask(255,255,255,255)}
+		}
+	}
 	return Result, err
 
 }
